4836270ffaf831a5dafe966e9e1c119d84ea53cd
Taha. Dostifam <mr.tahadostifam@gmail.com>
2025-11-22 18:42:51 +0330

    changing colorscheme

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -22,7 +22,33 @@ return {
                     variables = {},
                 },
                 on_colors = function(colors)
-                    colors.bg = "#000000"
+                    colors.bg       = "#000000"
+                    colors.terminal = {
+                        black          = "#000000",
+                        black_bright   = "#5B5D64",
+                        blue           = "#0554FF",
+                        blue_bright    = "#2063FF",
+                        cyan           = "#02A2FF",
+                        cyan_bright    = "#60BFFF",
+                        green          = "#5FB801",
+                        green_bright   = "#B2FD48",
+                        magenta        = "#6512FF",
+                        magenta_bright = "#8746FF",
+                        red            = "#E7002A",
+                        red_bright     = "#DF556C",
+                        white          = "#ECF0FF",
+                        white_bright   = "#FFFFFF",
+                        yellow         = "#FF9900",
+                        yellow_bright  = "#FFBA43",
+                    }
+                    colors.todo     = "#1660FF"
+                    colors.blue     = "#0554FF"
+                    colors.blue0    = "#3D59A1"
+                    colors.blue1    = "#2AC3DE"
+                    colors.blue2    = "#0DB9D7"
+                    colors.blue5    = "#89DDFF"
+                    colors.blue6    = "#B4F9F8"
+                    colors.blue7    = "#394B70"
                 end,
                 on_highlights = function(hl, c)
                     hl.CursorLine = { bg = "#6b6a6a", bold = true }
@@ -40,7 +66,8 @@ return {
         require("lualine").setup({
             options = {
                 icons_enabled = true,
-                theme = "tokyonight"
+                theme = "tokyonight",
+                globalstatus = true
             },
             sections = {
                 lualine_a = {
@@ -60,7 +87,7 @@ return {
         config = function()
             require("nvim-tree").setup({
                 view = {
-                    width = 30,
+                    width = 25,
                     side = "left",
                 },
                 renderer = {
@@ -76,14 +103,14 @@ return {
                 filters = {
                     dotfiles = false,
                 },
+                actions = {
+                    open_file = {
+                        quit_on_open = false
+                    }
+                }
             })
 
-            vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-
-            vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NvimTreeVertSplit", { bg = "none", fg = "#000000" })
+            vim.keymap.set("n", "<C-S-e>", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
         end,
     }
 }
